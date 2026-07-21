467a00aff2970bbc2da9f000255580df144fd89f
Sudeepsd <53992202+Sudeepsd@users.noreply.github.com>
2026-07-21 16:08:25 +0530

    Create template2.cpp

---
+++ template2.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define per(i, a, b) for(int i = (a); i >= (b); --i)
+#define each(x, a) for (auto& x : a)
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<pii> vp;
+
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+// const int NTT = 998244353;
+const int dirs = {0, 1, 0, -1, 0}
+
+#define YES cout << "YES\n";
+#define NO cout << "NO\n";
+
+template<typename T>
+void read(vector<T>& v) {
+    for (auto &x : v) cin >> x;
+}
+
+template<typename T>
+void print(const vector<T>& v) {
+    for (auto &x : v) cout << x << ' ';
+    cout << '\n';
+}
+
+void solve() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc = 1;
+    // cin >> tc;
+
+    while (tc--) {
+        solve();
+    }
+}
